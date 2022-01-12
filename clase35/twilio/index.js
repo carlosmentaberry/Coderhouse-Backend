6e5ce45dfa87fd937c515b351214b82db7bc4b39
Charly <cmmentaberry@gmail.com>
2022-01-11 23:35:44 -0300

    clase 34 y 35

--- clase35/twilio/index.js
+++ clase35/twilio/index.js
@@ -4,8 +4,8 @@ const twilio = require("twilio");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const accountid = "asdfqrwe";
-const authToken = "asdfqrwe";
+const accountid = process.env.accountid;
+const authToken = process.env.authToken;
 
 const client = twilio(accountid, authToken);
 
@@ -13,8 +13,8 @@ app.post("/twilio", async (req, res) => {
     try{
         const message = await client.messages.create({
             body:"hola test twilio desde nodejs",
-            from:"nrodetwilio-phonenumber",
-            to:"+5491136699432"
+            from:process.env.sms_from,
+            to:process.env.sms_to
         })
         console.log(message);
         res.send("txt enviado");
